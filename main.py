3b5b823f543265a0d4e95f4deed9a221dbb655c2
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2026-01-30 09:12:21 -0800

    added origins

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",   # Vite frontend
+    "http://127.0.0.1:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],  # your frontend URL
